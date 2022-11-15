65341eb2ba82681efea9fe89964d39934781da42
Abraham Mejicanos <amejicanos38@gmail.com>
2022-11-15 00:32:00 -0600

    Creacion Visual Completa

---
+++ PruebaCapas/ProyectoPrueba/Principal/FrmUsuario.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace ProyectoPrueba.Principal
+{
+    partial class FrmUsuario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tbN = new System.Windows.Forms.TextBox();
+            this.tbPA = new System.Windows.Forms.TextBox();
+            this.tbSA = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.DtpFN = new System.Windows.Forms.DateTimePicker();
+            this.chkE = new System.Windows.Forms.CheckBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 185);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(776, 177);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // tbN
+            // 
+            this.tbN.Location = new System.Drawing.Point(12, 35);
+            this.tbN.Name = "tbN";
+            this.tbN.Size = new System.Drawing.Size(100, 20);
+            this.tbN.TabIndex = 1;
+            // 
+            // tbPA
+            // 
+            this.tbPA.Location = new System.Drawing.Point(138, 35);
+            this.tbPA.Name = "tbPA";
+            this.tbPA.Size = new System.Drawing.Size(100, 20);
+            this.tbPA.TabIndex = 2;
+            // 
+            // tbSA
+            // 
+            this.tbSA.Location = new System.Drawing.Point(265, 35);
+            this.tbSA.Name = "tbSA";
+            this.tbSA.Size = new System.Drawing.Size(100, 20);
+            this.tbSA.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(135, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Primer Apellido";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(262, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Segundo Apellido";
+            // 
+            // DtpFN
+            // 
+            this.DtpFN.Location = new System.Drawing.Point(80, 81);
+            this.DtpFN.Name = "DtpFN";
+            this.DtpFN.Size = new System.Drawing.Size(285, 20);
+            this.DtpFN.TabIndex = 7;
+            // 
+            // chkE
+            // 
+            this.chkE.AutoSize = true;
+            this.chkE.Location = new System.Drawing.Point(15, 84);
+            this.chkE.Name = "chkE";
+            this.chkE.Size = new System.Drawing.Size(59, 17);
+            this.chkE.TabIndex = 8;
+            this.chkE.Text = "Estado";
+            this.chkE.UseVisualStyleBackColor = true;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(509, 156);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 9;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(611, 156);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(75, 23);
+            this.btnActualizar.TabIndex = 10;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(713, 156);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 11;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // FrmUsuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 379);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.chkE);
+            this.Controls.Add(this.DtpFN);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbSA);
+            this.Controls.Add(this.tbPA);
+            this.Controls.Add(this.tbN);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "FrmUsuario";
+            this.Text = "FrmUsuario";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox tbN;
+        private System.Windows.Forms.TextBox tbPA;
+        private System.Windows.Forms.TextBox tbSA;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker DtpFN;
+        private System.Windows.Forms.CheckBox chkE;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnEliminar;
+    }
+}
